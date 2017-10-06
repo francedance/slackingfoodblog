bf774233821e00b135f317b73bed1482da42d74e
Jae Hyuk Oh <jaehyuko@yahoo.com>
2017-10-06 11:03:08 -0400

    Update my_food_journey.js

--- models/my_food_journey.js
+++ models/my_food_journey.js
@@ -1,20 +1,14 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
-
-
 var myfoodjourneySchema = new Schema({
 
     title: String,
-     images: [ String, String, String, String, String],
+    images: [ String, String, String, String, String],
     content: String,
     updated: {type: Date, default: Date.now }
     
 });
 
-
-
 var myfoodjourney = mongoose.model('myfoodjourney', myfoodjourneySchema);
-
-
 module.exports = myfoodjourney;
