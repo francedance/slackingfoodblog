8aa95e12ccb35bd2abd4e8bc90a80a46d8b16c65
Jae Hyuk Oh <jaehyuko@yahoo.com>
2017-10-06 11:03:52 -0400

    Update what_i_eat_recipe.js

--- models/what_i_eat_recipe.js
+++ models/what_i_eat_recipe.js
@@ -1,20 +1,14 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
-
-
 var whatieatrecipeSchema = new Schema({
 
     title: String,
-     images: [ String, String, String, String, String],
+    images: [ String, String, String, String, String],
     content: String,
     updated: {type: Date, default: Date.now }
     
 });
 
-
-
 var whatieatrecipe = mongoose.model('whatieatrecipe', whatieatrecipeSchema);
-
-
 module.exports = whatieatrecipe;
