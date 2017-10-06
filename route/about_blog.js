27e53ae09172f2629f3e3fd0d52d23cb4418b6a3
Jae Hyuk Oh <jaehyuko@yahoo.com>
2017-10-06 11:04:33 -0400

    Update about_blog.js

--- route/about_blog.js
+++ route/about_blog.js
@@ -1,8 +1,6 @@
 var express = require('express');
 var router = express.Router();
 
-
-
 router.get('/',function(req,res){
 
 var session = req.session;
@@ -10,8 +8,5 @@ var session = req.session;
 res.render('about_blog', {session});
 res.end();
 });
-
-
-
 
 module.exports = router;
