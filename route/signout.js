d8f15878e29f70838b385df7a4c3b09b259de694
Jae Hyuk Oh <jaehyuko@yahoo.com>
2017-10-06 11:16:54 -0400

    Update signout.js

--- route/signout.js
+++ route/signout.js
@@ -3,16 +3,10 @@ var router = express.Router();
 var cookieParser = require('cookie-parser');
 var session = require('cookie-session');
 
-
-
-
 router.get('/', function(req,res){
-
-    
    req.session = null;
     res.redirect('/');
     res.end();
-
 })
 
 module.exports = router;
