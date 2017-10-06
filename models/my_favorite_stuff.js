0701f3d73af9f574e576a2bde6be9141aec2dfcd
Jae Hyuk Oh <jaehyuko@yahoo.com>
2017-10-06 11:02:47 -0400

    Update my_favorite_stuff.js

--- models/my_favorite_stuff.js
+++ models/my_favorite_stuff.js
@@ -1,20 +1,14 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
-
-
 var myfavoritestuffSchema = new Schema({
 
     title: String,
-     images: [ String, String, String, String, String],
+    images: [ String, String, String, String, String],
     content: String,
-     updated: {type: Date, default: Date.now }
+    updated: {type: Date, default: Date.now }
     
 });
 
-
-
 var myfavoritestuff = mongoose.model('myfavoritestuff', myfavoritestuffSchema);
-
-
 module.exports = myfavoritestuff;
