bf01f749a8aee6ab359f667fd93230ccb1860291
Jae Hyuk Oh <jaehyuko@yahoo.com>
2017-10-06 11:11:12 -0400

    Update dashboard.js

--- route/dashboard.js
+++ route/dashboard.js
@@ -1,12 +1,8 @@
 var express = require('express');
 var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
-
 var uri = process.env.MONGODB_URI;
-//mongoose.connect(process.env.MONGODB_URI);
 mongoose.connect(uri);
-
-
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var session = require('cookie-session');
@@ -16,9 +12,6 @@ var util = require('util');
 var multer = require('multer');
 var fs = require('fs-extra');
 
-//var key = process.env.API_KEY;
-//var secret = process.env.API_SECRET;
-
 var key = process.env.API_KEY;
 var secret = process.env.API_SECRET;
 
@@ -26,24 +19,18 @@ var Myfavoritestuff = require('../models/my_favorite_stuff.js');
 var Myfoodjourney = require('../models/my_food_journey.js');
 var Whatieatrecipe = require('../models/what_i_eat_recipe.js');
 
-
-
 var router = express.Router();
-
 
 router.use(bodyParser.urlencoded({extended: true}));
 
 router.use(session({
       name: "admin",
-      secret: "Linux1994", 
+      secret: process.env.SECRET, 
                     expires: new Date(Date.now() + (30 * 86400 * 1000)),
                 cookie: {maxAge: new Date(Date.now()+ 600000 )}
             
     }));
            
-
-
-
 router.get('/',function(req,res){
 
     var session = req.session;
@@ -53,18 +40,13 @@ router.get('/',function(req,res){
         
     }else{
         res.render("dashboard",{session});
-        res.end();
-        
+        res.end();  
     }
-    
-
 });
 
 var upload = multer({dest: './upload'});
 
 router.post('/upload_to_my_favorite_stuff' , upload.array('imagefile', 5) , function(req,res){
-
-    
 
     if(req.files){
     var oldpath = [];
@@ -74,34 +56,24 @@ router.post('/upload_to_my_favorite_stuff' , upload.array('imagefile', 5) , func
     var imagename = [];
     var img_url = [];
 
-   
-
     for (i = 0 ; i < req.files.length ; i++){
         oldpath[i] = req.files[i].path;
         newpath[i] = "./upload/" + req.files[i].originalname;
-       
-       
         imagename[i] = req.files[i].originalname;
-       
     }
    
-    
-   
     for(i = 0 ; i < req.files.length ; i++){
-   fs.rename(oldpath[i], newpath[i], function (err) {
-       if(err) throw err;
+            fs.rename(oldpath[i], newpath[i], function (err) {
+                  if(err) throw err;
   
-   });
+        });
     }
 
       var post = new Myfavoritestuff ({
        title: title,
-      images: [imagename[0], imagename[1], imagename[2], imagename[3], imagename[4]],
+       images: [imagename[0], imagename[1], imagename[2], imagename[3], imagename[4]],
        content: content
-
-   });
-
-   
+      });
 
     cloudinary.config({ 
            cloud_name: 'slacking-food-blog', 
@@ -110,30 +82,24 @@ router.post('/upload_to_my_favorite_stuff' , upload.array('imagefile', 5) , func
            });
 
        for(i = 0 ; i < req.files.length ; i++){
-    cloudinary.uploader.upload(newpath[i],
-                function(result) { 
-               
+            cloudinary.uploader.upload(newpath[i],
+              function(result) { 
                console.log(result); 
                 }, 
                {
                    public_id: imagename[i],
-  
-               } );
-
+               });
         }
-
-
+          
    post.save(function(err){
       if(err) throw(err);
         console.log('post to my favorite stuff successfully saved!');
-        
      });
-
 
      for(i = 0 ; i < req.files.length ; i++) {
      del(['./upload/' + imagename[i]]);
-
      }
+          
      res.redirect('/my_favorite_stuff');
      res.end();
     }else{
@@ -145,46 +111,40 @@ router.post('/upload_to_my_favorite_stuff' , upload.array('imagefile', 5) , func
         title: title,
         imagename: imagename,
         content: content
-
          });
-
+          
            post.save(function(err){
                  if(err) throw(err);
                 console.log('post to my favorite stuff successfully saved!');
-        
              });
+          
          res.redirect('/my_favorite_stuff');
          res.end();
     }
 
-    
-
 });
 
 router.post('/upload_to_my_food_journey' , upload.array('imagefile',5) , function(req,res){
-    
-   
+
     if(req.files){
     var oldpath = [];
     var newpath = [];
     var title = req.body.title;
     var content = req.body.texts;
     var imagename = [];
-
-    
-
+          
     for (i = 0 ; i < req.files.length ; i++){
         oldpath[i] = req.files[i].path;
         newpath[i] = "./upload/" + req.files[i].originalname;
         imagename[i] = req.files[i].originalname;
     }
    
-   
+
     for(i = 0 ; i < req.files.length ; i++){
-   fs.rename(oldpath[i], newpath[i], function (err) {
-       if(err) throw err;
+            fs.rename(oldpath[i], newpath[i], function (err) {
+                  if(err) throw err;
   
-   });
+            });
     }
 
     var post = new Myfoodjourney ({
@@ -194,40 +154,38 @@ router.post('/upload_to_my_food_journey' , upload.array('imagefile',5) , functio
 
    });
 
-
     cloudinary.config({ 
            cloud_name: 'slacking-food-blog', 
             api_key: key, 
            api_secret: secret 
            });
-
-            
+  
         for(i = 0 ; i < req.files.length ; i++){
-    cloudinary.uploader.upload(newpath[i],
-                function(result) { 
-               console.log(result); }, 
-               {
-                   public_id: imagename[i],
+                   cloudinary.uploader.upload(newpath[i],
+                               function(result) { 
+                                    console.log(result); }, 
+                                    {
+                                      public_id: imagename[i],
                    
-               } );
+                                    });
 
         }
     
    post.save(function(err){
       if(err) throw(err);
         console.log('post to my food journey successfully saved!');
-        
      });
-
 
      for(i = 0 ; i < req.files.length ; i++) {
      del(['./upload/' + imagename[i]]);
-
      }
+          
      res.redirect('/my_food_journey');
      res.end();
+          
     }else{
-         var title = req.body.title;
+          
+        var title = req.body.title;
         var content = req.body.texts;
         var imagename = undefined;
 
@@ -240,49 +198,43 @@ router.post('/upload_to_my_food_journey' , upload.array('imagefile',5) , functio
 
            post.save(function(err){
                  if(err) throw(err);
-                console.log('post to my food journey successfully saved!');
-        
+                       console.log('post to my food journey successfully saved!');
              });
+          
          res.redirect('/my_food_journey');
          res.end();
+          
     }
-
-
 });
 
 router.post('/upload_to_what_i_eat_recipe' , upload.array('imagefile',5) , function(req,res){
     
-    
     if(req.files){
-    var oldpath = [];
-    var newpath = [];
-    var title = req.body.title;
-    var content = req.body.texts;
-    var imagename = [];
-
-   
+          var oldpath = [];
+          var newpath = [];
+          var title = req.body.title;
+          var content = req.body.texts;
+          var imagename = [];
 
     for (i = 0 ; i < req.files.length ; i++){
-        oldpath[i] = req.files[i].path;
-        newpath[i] = "./upload/" + req.files[i].originalname;
-        imagename[i] = req.files[i].originalname;
-    }
+              oldpath[i] = req.files[i].path;
+              newpath[i] = "./upload/" + req.files[i].originalname;
+              imagename[i] = req.files[i].originalname;
+      }
    
    
     for(i = 0 ; i < req.files.length ; i++){
-   fs.rename(oldpath[i], newpath[i], function (err) {
-       if(err) throw err;
+      fs.rename(oldpath[i], newpath[i], function (err) {
+           if(err) throw err;
   
-   });
+      });
     }
 
     var post = new Whatieatrecipe({
        title: title,
-      images: [imagename[0], imagename[1], imagename[2], imagename[3], imagename[4]],
+       images: [imagename[0], imagename[1], imagename[2], imagename[3], imagename[4]],
        content: content
-
    });
-
 
     cloudinary.config({ 
            cloud_name: 'slacking-food-blog', 
@@ -346,9 +298,5 @@ router.get('*',function(req,res){
     //error code, 404
 
 });
-
-
-
-
 
 module.exports = router;
