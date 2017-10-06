1cb4409ada766adf67cab21e0801e8a05117fb42
Jae Hyuk Oh <jaehyuko@yahoo.com>
2017-10-06 11:13:39 -0400

    Update login.js

--- route/login.js
+++ route/login.js
@@ -2,34 +2,21 @@ var express = require('express');
 var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
 var bcrypt = require('bcryptjs');
-
 var uri = process.env.MONGODB_URI;
 mongoose.connect(uri);
-
-
-
-
 var cookieParser = require('cookie-parser');
 var session = require('cookie-session');
-
-
 var Admin = require('../models/admin.js');
-
 var router = express.Router();
-
-
 
 router.use(session({
       name: "admin",
-      secret: "Linux1994", 
+      secret: process.env.SECRET, 
                     expires: new Date(Date.now() + (30 * 86400 * 1000)),
                 cookie: {maxAge: new Date(Date.now()+ 60000 )}
             
     }));
            
-
-
-
 router.get('/',function(req,res){
 
     var session = req.session;
@@ -45,12 +32,9 @@ router.post('/validate',function(req,res){
             var password = req.body.password;
             var session = req.session;
            var lookfor;
-
             
             function validation(lookfor) {
                      Admin.find({ $and: [{'username': { $eq: username}} , {'password': { $eq: lookfor }}] }, function(err,result){
-
-                           
 
                             if(err){
                                 throw err;
@@ -64,19 +48,12 @@ router.post('/validate',function(req,res){
                                 res.redirect('/dashboard');
                                 res.end();
                             }
-                                
 
-                        
                         });
-                    
-
             }
-
-
-
+      
                 Admin.findById('598a249ff36d28307e603d1c',function(err, admin){
                     if (err) throw err;
-
                     bcrypt.compare(password , admin.password , function(err, result){
                         if(result){
                             validation(admin.password);
