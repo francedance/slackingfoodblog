8743b1612bffcdad8b79793f18b518713b688d18
Jae Hyuk Oh <jaehyuko@yahoo.com>
2017-10-08 01:11:33 -0400

    Update index.js

--- index.js
+++ index.js
@@ -11,16 +11,15 @@ app.use(cookieParser());
 app.use(session({
       name: 'admin',
       secret: process.env.SECRET, 
-                    expires: new Date(Date.now() + (30 * 86400 * 1000)),
-                cookie: {maxAge: new Date(Date.now()+ 600000 )}
-            
+      expires: new Date(Date.now() + (30 * 86400 * 1000)),
+      cookie: {maxAge: new Date(Date.now()+ 600000 )}      
     }));
 
 var index = require('./route/index.js');
-var about_blog = require('./route/about_blog.js');
-var what_i_eat_recipes = require('./route/what_i_eat_recipes.js');
-var my_food_journey = require('./route/my_food_journey.js');
-var my_favorite_stuff = require('./route/my_favorite_stuff.js');
+var AboutBlog = require('./route/about_blog.js');
+var WhatIEatRecipes = require('./route/what_i_eat_recipes.js');
+var MyFoodJourney = require('./route/my_food_journey.js');
+var MyFavoriteStuff = require('./route/my_favorite_stuff.js');
 var login = require('./route/login.js');
 var signout = require('./route/signout.js');
 var dashboard = require('./route/dashboard.js');
@@ -30,23 +29,23 @@ app.set('view engine', 'ejs');
 app.set('views','./views');
 //setting up some routes
 app.use('/',index);
-app.use('/about_blog',about_blog);
-app.use('/what_i_eat_recipes',what_i_eat_recipes);
-app.use('/my_food_journey',my_food_journey);
-app.use('/my_favorite_stuff',my_favorite_stuff);
+app.use('/about_blog',AboutBlog);
+app.use('/what_i_eat_recipes',WhatIEatRecipes);
+app.use('/my_food_journey',MyFoodJourney);
+app.use('/my_favorite_stuff',MyFavoriteStuff);
 app.use('/login',login);
 app.use('/signout',signout);
 app.use('/dashboard',dashboard);
 
 app.get('/index', function(req,res){
       var session = req.session; 
-     res.redirect('/');
-     res.end();
+      res.redirect('/');
+      res.end();
 });
 
 app.get('*',function(req,res){
-    res.status(404).render('error_page');
-    res.end();
+      res.status(404).render('error_page');
+      res.end();
     //error code, 404
 });
 
