cc5c9d432fc66b558f991639c7799625ba218a83
Jae Hyuk Oh <jaehyuko@yahoo.com>
2017-10-08 02:23:49 -0400

    Update what_i_eat_recipes.js

--- route/what_i_eat_recipes.js
+++ route/what_i_eat_recipes.js
@@ -13,19 +13,21 @@ router.get('/',function(req,res , next){
     var session = req.session;
 
    Whatieatrecipe.find({}).sort({updated: -1}).limit(8).exec(function(err,posts){
-       
-            if(err) {
-                res.send(err);
-                res.end();
-            }else{
-
-                if(session.username){
+            if(err)
+            {
+              res.send(err);
+              res.end();
+            }else
+            {
+              if(session.username)
+              {
                 res.render('what_i_eat_recipes_editable', {session, posts});
                 res.end();
-                }else{
+              }else
+              {
                 res.render('what_i_eat_recipes', {session, posts});
                 res.end();
-                }
+              }
             }
         });
 });
@@ -35,39 +37,42 @@ router.get('/:page_number',function(req,res){
         var session = req.session;
         var skip_count = req.params.page_number * 8;
     
-       Whatieatrecipe.find({}).sort({updated: -1}).limit(8).skip(skip_count).exec(function(err,posts){
-                
-                if(err) {
-                    res.send(err);
-                    res.end();
-                }else{
-                    if(session.username){
+        Whatieatrecipe.find({}).sort({updated: -1}).limit(8).skip(skip_count).exec(function(err,posts){
+                if(err)
+                {
+                  res.send(err);
+                  res.end();
+                }else
+                {
+                  if(session.username)
+                  {
                     res.render('what_i_eat_recipes_editable', {session, posts});
                     res.end();
-                    }else{
-                        var page_number = req.params.page_number;
-                        console.log('from lists: ' + posts);
-                    res.render('what_i_eat_recipes_' + page_number, {session, posts});
-                    res.end();
+                    }else
+                    {
+                      var page_number = req.params.page_number;
+                      res.render('what_i_eat_recipes_' + page_number, {session, posts});
+                      res.end();
                     }
-                }
+                   }
             });
     });
 
 router.get('/delete/:id', function(req,res){
 
         Whatieatrecipe.findById(req.params.id , function(err,post){
-            if(err) {
-                res.send(err);
-                res.end();
-            }else{
-
-                post.remove(function(err){
-                if(err) throw err;
-
-                else{
-                    res.redirect('/what_i_eat_recipes');
-                    res.end();
+            if(err)
+            {
+              res.send(err);
+              res.end();
+            }else
+            {
+              post.remove(function(err){
+              if(err) throw err;
+                else
+                {
+                  res.redirect('/what_i_eat_recipes');
+                  res.end();
                 }
             });
         }      
